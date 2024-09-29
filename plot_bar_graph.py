3929118687a07ca50d40b7cb500d4939f7fa50f1
trivikram-06 <150177060+trivikram-06@users.noreply.github.com>
2024-09-29 10:46:02 +0530

    plot_bar_graph.py

--- plot_bar_graph.py
+++ plot_bar_graph.py
@@ -1,14 +1,10 @@
-# Import necessary libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load the dataset with cluster assignments
 dataset = pd.read_csv('Mall_Customers_with_Clusters.csv')
 
-# Calculate average spending score by cluster
 avg_spending_by_income = dataset.groupby('Cluster')['Spending Score (1-100)'].mean()
 
-# Plot Bar Graph for Spending Score by Income Bracket
 avg_spending_by_income.plot(kind='bar', figsize=(10, 6))
 plt.title('Average Spending Score by Income Cluster')
 plt.ylabel('Average Spending Score')
